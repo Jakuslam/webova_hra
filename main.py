e396838c353629ae41a1c35fec77620fff75482e
Jakub Sláma <krubas59@gmail.com>
2023-09-26 09:30:46 +0200

    basic setup

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from urllib import request
-from flask import Blueprint, render_template, request, flash
-import os
+from flask import render_template, request
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
-from werkzeug.utils import secure_filename
 from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'weby'
 
-cestaKeSlozece = r"path...." #Change variable cestaKeSložce to your patho to that folther.
-
-e = "newMember@gmail.com"
-p = "123"
 
 
 class UploadFileForm(FlaskForm):
